4a952b3aa4b69aefc3fcbdce313d3030ff27a695
Chindanai Wasanachin <dn.chindanai@gmail.com>
2017-04-09 01:09:39 +0700

    Create Drug table for drug page

--- app/src/main/java/dev/medicine/DatabaseHelper.java
+++ app/src/main/java/dev/medicine/DatabaseHelper.java
@@ -3,18 +3,47 @@ package dev.medicine;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME="constants.db";
-    private static final int SCHEMA=1;
-    static final String TITLE="title";
-    static final String VALUE="value"; static final String TABLE="constants";
+
+    private static final String TAG = "Database";
+    private static final String DATABASE_NAME="medicine";
+    private static final int DATABASE_VERSION=1;
+
+
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, SCHEMA);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        createDrugTable(db);
+    }
+
+    //Create Drug Table
+    private void createDrugTable(SQLiteDatabase db) {
+        Log.i(TAG, "Drug Table Creating");
+        //Specific Table Column and Table name
+        String columnId = "id";
+        String columnDrugName = "name";
+        String columnHowToUse = "how_to";
+        String columnRisk = "risk";
+        String columnNote = "note";
+        String columnReference = "reference";
+        String drugTable="drug";
+
+
+        String sqlCreateTable = "CREATE TABLE IF NOT EXISTS " + drugTable + " ("
+                + columnId + "INTEGER PRIMARY KEY NOT NULL, "
+                + columnDrugName + "VARCHAR, "
+                + columnHowToUse + "TEXT, "
+                + columnRisk + "VARCHAR, "
+                + columnNote + "TEXT, "
+                + columnReference + "TEXT";
+
+        db.execSQL(sqlCreateTable);
+        Log.i(TAG, "Drug Table Created");
     }
 
     @Override
