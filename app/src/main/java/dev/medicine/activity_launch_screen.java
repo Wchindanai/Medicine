2e1d0f8359220e738c4080bc459602267f28f747
Chindanai Wasanachin <dn.chindanai@gmail.com>
2017-04-13 02:16:56 +0700

    Config Splash Screen

--- app/src/main/java/dev/medicine/activity_launch_screen.java
+++ app/src/main/java/dev/medicine/activity_launch_screen.java
@@ -3,8 +3,10 @@ package dev.medicine;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class activity_launch_screen extends AppCompatActivity {
 
@@ -14,6 +16,8 @@ public class activity_launch_screen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_screen);
         ImageButton start_btn = (ImageButton)findViewById(R.id.icon_start);
+        TextView applicationDetail = (TextView)findViewById(R.id.detail);
+        applicationDetail.setText(R.string.application_detail);
 
         start_btn.setOnClickListener(new View.OnClickListener(){
             @Override
