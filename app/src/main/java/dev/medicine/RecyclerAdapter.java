269df8f96c291b00447875a47fb7b306f36a0d62
Chindanai Wasanachin <dn.chindanai@gmail.com>
2017-04-13 01:54:32 +0700

    Recycle View edit for drug model and complete drug model for fetch data from database

---
+++ app/src/main/java/dev/medicine/RecyclerAdapter.java
@@ -0,0 +1,69 @@
+package dev.medicine;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import static android.R.attr.name;
+
+/**
+ * Created by dream on 4/13/2017 AD.
+ */
+
+class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+
+    private final Context context;
+    private final List<DrugModel> drug_list;
+
+    public RecyclerAdapter(Context context, List<DrugModel> drug_list) {
+        this.context = context;
+        this.drug_list = drug_list;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recycleview, parent, false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return  viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.drugImage.setImageResource(R.drawable.icon_drug_s);
+        holder.drugName.setText(drug_list.get(position).getName());
+        holder.drugRisk.setText(drug_list.get(position).getRisk());
+    }
+
+    @Override
+    public int getItemCount() {
+        return drug_list.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView drugImage;
+        public TextView drugName;
+        public TextView drugRisk;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            drugImage = (ImageView)itemView.findViewById(R.id.item_image);
+            drugName = (TextView)itemView.findViewById(R.id.item_name);
+            drugRisk = (TextView)itemView.findViewById(R.id.item_risk);
+
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/dev/medicine/activity_drug.java
+++ app/src/main/java/dev/medicine/activity_drug.java
@@ -2,17 +2,41 @@ package dev.medicine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class activity_drug extends AppCompatActivity {
+
+    //Setting Model Drug
+    private List<DrugModel> drug_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drug);
+
+        drug_list = new ArrayList<>();
+
+        //Init Recycler View And Set Adapter
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.hasFixedSize();
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setAdapter(new RecyclerAdapter(this, drug_list));
+
+        DrugModel drug = new DrugModel(1, "Paracetamol", "Eat", "Low", " ", " ");
+        drug_list.add(drug);
+
+        drug = new DrugModel(2, "Nogesic", "Eat", "High", "Test", "Test");
+        drug_list.add(drug);
+
+
     }
 
     @Override
